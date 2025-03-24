9da056ebede340bda7bca5b45ffa327292466afe
francisco-the-man <avery@louisfamily.com>
2025-03-24 00:26:30 -0700

    Setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
-  }
+  },
+  output: 'export',
+  basePath: '/sisely-web',
+  assetPrefix: '/sisely-web/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
